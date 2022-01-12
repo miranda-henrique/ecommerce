5d4154542ded704a315c0beb4e52dfab3dde7da7
Henrique Miranda <miranda.henrique@outlook.com.br>
2022-01-12 15:55:32 -0300

    Add Header, Footer, react-bootstrap, bootswatch and fa-icons

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <header>
+            <Navbar
+                bg="dark"
+                variant="dark"
+                expand="lg"
+                collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href="/">
+                        MERN Ecommerce
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="/cart">
+                                <i className="fas fa-shopping-cart me-1"></i>
+                                Cart
+                            </Nav.Link>
+                            <Nav.Link href="/login">
+                                <i className="fas fa-user me-1"></i>
+                                Sign In
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
+}
+
+export default Header;
