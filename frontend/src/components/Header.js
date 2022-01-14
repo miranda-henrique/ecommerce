253a9fcdb56f4a3fc7bcba3dcf9acd2933fca638
Henrique Miranda <miranda.henrique@outlook.com.br>
2022-01-14 19:19:24 -0300

    ProductScreen component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
@@ -10,20 +11,26 @@ const Header = () => {
                 expand="lg"
                 collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">
-                        MERN Ecommerce
-                    </Navbar.Brand>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>
+                            MERN Ecommerce
+                        </Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link href="/cart">
-                                <i className="fas fa-shopping-cart me-1"></i>
-                                Cart
-                            </Nav.Link>
-                            <Nav.Link href="/login">
-                                <i className="fas fa-user me-1"></i>
-                                Sign In
-                            </Nav.Link>
+                            <LinkContainer to="/cart">
+                                <Nav.Link>
+                                    <i className="fas fa-shopping-cart me-1"></i>
+                                    Cart
+                                </Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/login">
+                                <Nav.Link href="/login">
+                                    <i className="fas fa-user me-1"></i>
+                                    Sign In
+                                </Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
