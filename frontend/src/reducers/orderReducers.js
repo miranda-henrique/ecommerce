6b57c979f7b92ade743204f9ca7c860c290b566a
Henrique Miranda <miranda.henrique@outlook.com.br>
2022-02-20 16:38:48 -0300

    Order details reducer and action

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -2,6 +2,9 @@ import {
     ORDER_CREATE_REQUEST,
     ORDER_CREATE_SUCCESS,
     ORDER_CREATE_FAIL,
+    ORDER_DETAILS_REQUEST,
+    ORDER_DETAILS_SUCCESS,
+    ORDER_DETAILS_FAIL,
 } from '../constants/orderConstants';
 
 
@@ -27,5 +30,33 @@ const orderCreateReducer = (state = {}, action) => {
     }
 };
 
+const orderDetailsReducer = (
+    state = {
+        orderItems: [],
+        shippingAddress: {},
+    },
+    action
+) => {
+    switch (action.type) {
+        case ORDER_DETAILS_REQUEST:
+            return {
+                ...state,
+                loading: true,
+            };
+        case ORDER_DETAILS_SUCCESS:
+            return {
+                loading: false,
+                order: action.payload,
+            };
+        case ORDER_DETAILS_FAIL:
+            return {
+                loading: false,
+                error: action.payload,
+            };
+        default:
+            return state;
+    }
+};
 
-export { orderCreateReducer };
+
+export { orderCreateReducer, orderDetailsReducer };
